f67f81acade528d97c189b8a3673db64236d04f9
Vagish Maurya <vmaurya@calance.com>
2024-07-05 10:31:03 +0530

    removed babel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import babel from 'vite-plugin-babel';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
